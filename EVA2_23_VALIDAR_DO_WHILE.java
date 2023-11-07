119d124bc7841f47bde534358adacd60d69f1b60
DulNaomi <143562926+DulNaomi@users.noreply.github.com>
2023-11-07 17:51:34 -0600

    Add files via upload

---
+++ EVA2_23_VALIDAR_DO_WHILE.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_23_validar_do_while;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_23_VALIDAR_DO_WHILE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Scanner nono=new Scanner(System.in);
+        int valor;
+        String cade;
+        
+        do{
+            System.out.println("introduce un valor enter: ");
+            valor=nono.nextInt();
+        }while(true);
+    }
+    
+}
